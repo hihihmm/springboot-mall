fc78e2f9bb044bb42e8a6f94ee4ed85b3cf39ced
hihihmm <freedom80265@gmail.com>
2022-06-20 17:41:05 +0800

    商品功能 - 實作修改商品

--- springboot-mall/src/main/java/com/hihih/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/hihih/springbootmall/controller/ProductController.java
@@ -36,4 +36,23 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+
+        Product product = productService.getProductById(productId);
+
+        // [嚴謹] 先檢查 product 是否存在     (有無檢查皆可)
+        if(product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        // 如果存在，修改商品的數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updateProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
 }
