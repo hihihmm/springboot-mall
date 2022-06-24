b1f8f95240b94dbc1df78e784e8fa178652db315
hihihmm <freedom80265@gmail.com>
2022-06-24 14:38:25 +0800

    商品功能 - 實作查詢商品列表(排序)

--- springboot-mall/src/main/java/com/hihih/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/hihih/springbootmall/controller/ProductController.java
@@ -22,12 +22,19 @@ public class ProductController {
     // 不管 product 是否為 null，皆回傳 200 OK
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
+            // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
-            @RequestParam(required = false) String search
+            @RequestParam(required = false) String search,
+
+            // 排序 Sorting
+            @RequestParam(defaultValue = "created_date") String orderBy,
+            @RequestParam(defaultValue = "desc") String sort
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
 
         List<Product> prodcutList = productService.getProducts(productQueryParams);
 
