8051d3ced474bc92bb27d5a963a0d0855237a1af
hihihmm <freedom80265@gmail.com>
2022-12-06 19:46:53 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- src/main/java/com/hhihi/springbootmall/dao/ProductDao.java
+++ src/main/java/com/hhihi/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.hhihi.springbootmall.dao;
 import com.hhihi.springbootmall.dto.ProductRequest;
 import com.hhihi.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/hhihi/springbootmall/service/ProductService.java
+++ src/main/java/com/hhihi/springbootmall/service/ProductService.java
@@ -3,10 +3,14 @@ package com.hhihi.springbootmall.service;
 import com.hhihi.springbootmall.dto.ProductRequest;
 import com.hhihi.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
     void deleteProductById(Integer productId);
+
 }
